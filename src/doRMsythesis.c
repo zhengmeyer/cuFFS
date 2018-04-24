b2b89e8a1058fd275d3e32cdefe86777066c8979
Mattia Mancini <mancini@astron.nl>
2018-04-24 16:21:29 +0200

    fixing typos

--- src/doRMsythesis.c
+++ src/doRMsythesis.c
@@ -1,7 +1,7 @@
 
-#include<cuda_runtime.h>
-#include<cuda.h>
-#include<time.h>
+//#include<cuda_runtime.h>
+//#include<cuda.h>
+//#include<time.h>
 #include "structures.h"
 #include "constants.h"
 #include "devices.h"
@@ -12,17 +12,16 @@
 *
 *************************************************************/
 int allocateHostMemoryForComputation(float **lambdaDiff2Pointer, float **qImageArrayPointer, float **uImageArrayPointer,
-									 float **qPhi, float **uPhi, float **pPhi,
+									 float **qPhiPointer, float **uPhiPointer, float **pPhiPointer,
 									 int nInFrequencies, int nRA, int nPhi,  int nInElements, int nOutElements){
 	/* Allocate memory on the host */
-	t->startProc = clock();
 
-	*lambdaDiff2Pointer = (float *)calloc(nInFrequencies, sizeof(**lambdaDiff2));
-	*qImageArrayPointer = (float *)calloc(nInElements, sizeof(**qImageArray));
-	*uImageArrayPointer = (float *)calloc(nInElements, sizeof(**uImageArray));
-	*qPhiPointer = (float *)calloc(nOutElements, sizeof(**qPhi));
-	*uPhiPointer = (float *)calloc(nOutElements, sizeof(**uPhi));
-	*pPhiPointer = (float *)calloc(nOutElements, sizeof(**pPhi));
+	*lambdaDiff2Pointer = (float *)calloc(nInFrequencies, sizeof(**lambdaDiff2Pointer));
+	*qImageArrayPointer = (float *)calloc(nInElements, sizeof(**qImageArrayPointer));
+	*uImageArrayPointer = (float *)calloc(nInElements, sizeof(**uImageArrayPointer));
+	*qPhiPointer = (float *)calloc(nOutElements, sizeof(**qPhiPointer));
+	*uPhiPointer = (float *)calloc(nOutElements, sizeof(**uPhiPointer));
+	*pPhiPointer = (float *)calloc(nOutElements, sizeof(**pPhiPointer));
 	if(*lambdaDiff2Pointer == NULL || *qImageArrayPointer == NULL ||
 			*uImageArrayPointer == NULL || *qPhiPointer == NULL ||
 			*uPhiPointer == NULL || *pPhiPointer == NULL) {
@@ -32,8 +31,9 @@ int allocateHostMemoryForComputation(float **lambdaDiff2Pointer, float **qImageA
 
 }
 
-int computeLambdaSquareDifference(float lambdaDiff2, float *lambda2, float lambda20, size_t size){
+int computeLambdaSquareDifference(float  *lambdaDiff2, float *lambda2, float lambda20, size_t size){
 	/* Compute \lambda^2 - \lambda^2_0 once. Common for all threads */
+    int i;
 	for(i=0;i<size;i++) lambdaDiff2[i] = 2.0*(lambda2[i]-lambda20);
 }
 
@@ -46,11 +46,11 @@ int allocateDeviceMemoryForComputation(int cudaDeviceId, float **lambdaDiff2, fl
 	cudaGetDevice(&currentCudaDevice);
 
 	// set the desired device
-	cudaSetDevice(deviceId);
+	cudaSetDevice(cudaDeviceId);
 
 	/* Allocate memory on the device */
 	cudaMalloc(&lambdaDiff2, sizeof(**lambdaDiff2)*nFrequencies);
-	cudaMalloc(&phiAxis, sizeof(**phiAxis)*inOptions->nPhi);
+	cudaMalloc(&phiAxis, sizeof(**phiAxis)*nPhi);
 	cudaMalloc(&qImageArray, nInElements*sizeof(**qImageArray));
 	cudaMalloc(&uImageArray, nInElements*sizeof(**uImageArray));
 	cudaMalloc(&qPhi, nOutElements*sizeof(**qPhi));
@@ -69,7 +69,7 @@ int copyLambdaDifferenceToDevice(int deviceId, float *lambdaDiff2, float *device
 	cudaSetDevice(deviceId);
 
 	/* Transfer \lambda^2 - \lambda^2_0 to device */
-	cudaMemcpy(device_lambdaDiff2, lambdaDiff2, sizeof(*lambdaDiff)*length, cudaMemcpyHostToDevice);
+	cudaMemcpy(device_lambdaDiff2, lambdaDiff2, sizeof(*lambdaDiff2)*length, cudaMemcpyHostToDevice);
 	checkCudaError();
 
 	// switch back to the previous device
@@ -91,7 +91,7 @@ int copyPhiToDevice(int deviceId, float *phi, float *device_phi, size_t length){
 	cudaSetDevice(currentCudaDevice);
 }
 
-int copyStepToDevice(int deviceId, float *qImageArray, float *d_qImageArray float *uImageArray, float *d_uImageArray, size_t length){
+int copyStepToDevice(int deviceId, float *qImageArray, float *d_qImageArray, float *uImageArray, float *d_uImageArray, size_t length){
 	int currentCudaDevice;
 	// reads the current selected device and switch back after
 	cudaGetDevice(&currentCudaDevice);
@@ -99,10 +99,10 @@ int copyStepToDevice(int deviceId, float *qImageArray, float *d_qImageArray floa
 	cudaSetDevice(deviceId);
 
 	cudaMemcpy(d_qImageArray, qImageArray,
-	                  nInElements*sizeof(*qImageArray),
+	                  length*sizeof(*qImageArray),
 	                  cudaMemcpyHostToDevice);
 	cudaMemcpy(d_uImageArray, uImageArray,
-	                  nInElements*sizeof(*qImageArray),
+	                  length*sizeof(*qImageArray),
 	                  cudaMemcpyHostToDevice);
 	checkCudaError();
 
@@ -110,19 +110,16 @@ int copyStepToDevice(int deviceId, float *qImageArray, float *d_qImageArray floa
 	cudaSetDevice(currentCudaDevice);
 }
 
-int copyStepToDevice(int deviceId, float *qImageArray, float *d_qImageArray float *uImageArray, float *d_uImageArray, size_t length){
+int copyStepToHost(int deviceId, float *qPhi, float *d_qPhi, float *uPhi, float *d_uPhi, float *pPhi, float *d_pPhi, size_t length){
 	int currentCudaDevice;
 	// reads the current selected device and switch back after
 	cudaGetDevice(&currentCudaDevice);
 	// set the desired device
 	cudaSetDevice(deviceId);
 
-	cudaMemcpy(d_qImageArray, qImageArray,
-	                  nInElements*sizeof(*qImageArray),
-	                  cudaMemcpyHostToDevice);
-	cudaMemcpy(d_uImageArray, uImageArray,
-	                  nInElements*sizeof(*qImageArray),
-	                  cudaMemcpyHostToDevice);
+    cudaMemcpy(qPhi, d_qPhi, length*sizeof(*qPhi), cudaMemcpyDeviceToHost);
+    cudaMemcpy(uPhi, d_uPhi, length*sizeof(*qPhi), cudaMemcpyDeviceToHost);
+    cudaMemcpy(pPhi, d_pPhi, length*sizeof(*qPhi), cudaMemcpyDeviceToHost);
 	checkCudaError();
 
 	// switch back to the previous device
@@ -138,7 +135,7 @@ int copyStepToDevice(int deviceId, float *qImageArray, float *d_qImageArray floa
 int doRMSynthesis(struct optionsList *inOptions,
     struct IOFileDescriptors *descriptors,
     struct parameters *params,
-    strcut DataArrays *arrays,
+    struct DataArrays *data_arrays,
     struct deviceInfoList selectedDeviceInfo,
     struct timeInfoList *t) {
 
@@ -155,7 +152,7 @@ int doRMSynthesis(struct optionsList *inOptions,
     float *d_phiAxis;
 
     // Dimension sizes
-    int nFrequencies, int nRa, int nDec, int nPhi;
+    int nFrequencies, nRa, nDec, nPhi;
     long nInElements, nOutElements;
 
     dim3 calcThreadSize, calcBlockSize;
@@ -183,35 +180,35 @@ int doRMSynthesis(struct optionsList *inOptions,
        case HDF5:
           /* For HDF5, set up the hyperslab and data subset for input */
           dimIn = params->qAxisLen2 * params->qAxisLen3;
-          descriptors.qDataset   = H5Dopen2(descriptors->qFileh5, PRIMARYDATA, H5P_DEFAULT);
-          descriptors.qDataspace = H5Dget_space(qDataset);
-          descriptors.uDataset   = H5Dopen2(descriptors->uFileh5, PRIMARYDATA, H5P_DEFAULT);
-          descriptors.uDataspace = H5Dget_space(uDataset);
+          descriptors->qDataset   = H5Dopen2(descriptors->qFileh5, PRIMARYDATA, H5P_DEFAULT);
+          descriptors->qDataspace = H5Dget_space(descriptors->qDataset);
+          descriptors->uDataset   = H5Dopen2(descriptors->uFileh5, PRIMARYDATA, H5P_DEFAULT);
+          descriptors->uDataspace = H5Dget_space(descriptors->uDataset);
           countIn[0] = params->qAxisLen3;
           countIn[1] = 1; countIn[2] = params->qAxisLen2;
           offsetIn[0] = 0; offsetIn[1] = 0; offsetIn[2] = 0;
-          descriptors.qMemspace = H5Screate_simple(1, &dimIn, NULL);
-          descriptors.uMemspace = H5Screate_simple(1, &dimIn, NULL);
-          if( qDataset<0 || uDataset<0 || qDataspace<0 || uDataspace<0 || qMemspace<0 || uMemspace<0 )
+          descriptors->qMemspace = H5Screate_simple(1, &dimIn, NULL);
+          descriptors->uMemspace = H5Screate_simple(1, &dimIn, NULL);
+          if( descriptors->qDataset<0 || descriptors->uDataset<0 || descriptors->qDataspace<0 || descriptors->uDataspace<0 || descriptors->qMemspace<0 || descriptors->uMemspace<0 )
           { printf("\nError: HDF5 allocation failed\n"); }
 
           /* Set up the hyperslab and data subset for output */
           dimOut = params->qAxisLen2 * inOptions->nPhi;
-          descriptors.qOutDataset   = H5Dopen2(descriptors->qDirtyH5, PRIMARYDATA, H5P_DEFAULT);
-          descriptors.qOutDataspace = H5Dget_space(qOutDataset);
-          descriptors.uOutDataset   = H5Dopen2(descriptors->uDirtyH5, PRIMARYDATA, H5P_DEFAULT);
-          descriptors.uOutDataspace = H5Dget_space(uOutDataset);
-          descriptors.pOutDataset   = H5Dopen2(descriptors->pDirtyH5, PRIMARYDATA, H5P_DEFAULT);
-          descriptors.pOutDataspace = H5Dget_space(uOutDataset);
+          descriptors->qOutDataset   = H5Dopen2(descriptors->qDirtyH5, PRIMARYDATA, H5P_DEFAULT);
+          descriptors->qOutDataspace = H5Dget_space(descriptors->qOutDataset);
+          descriptors->uOutDataset   = H5Dopen2(descriptors->uDirtyH5, PRIMARYDATA, H5P_DEFAULT);
+          descriptors->uOutDataspace = H5Dget_space(descriptors->uOutDataset);
+          descriptors->pOutDataset   = H5Dopen2(descriptors->pDirtyH5, PRIMARYDATA, H5P_DEFAULT);
+          descriptors->pOutDataspace = H5Dget_space(descriptors->uOutDataset);
           countOut[0] = inOptions->nPhi;
-          countOut[1] = 1; countOut[2] = descriptors->qAxisLen2;
+          countOut[1] = 1; countOut[2] = params->qAxisLen2;
           offsetOut[0] = 0; offsetOut[1] = 0; offsetOut[2] = 0;
-          descriptors.qOutMemspace = H5Screate_simple(1, &dimOut, NULL);
-          descriptors.uOutMemspace = H5Screate_simple(1, &dimOut, NULL);
-          descriptors.pOutMemspace = H5Screate_simple(1, &dimOut, NULL);
-          if( descriptors.qOutDataset<0 || descriptors.uOutDataset<0 || descriptors.pOutDataset<0 ||
-              descriptors.qOutDataspace<0 || descriptors.uOutDataspace<0 || descriptors.pOutDataspace<0 ||
-              descriptors.qOutMemspace<0 || descriptors.uOutMemspace<0 || descriptors.pOutMemspace<0 ) {
+          descriptors->qOutMemspace = H5Screate_simple(1, &dimOut, NULL);
+          descriptors->uOutMemspace = H5Screate_simple(1, &dimOut, NULL);
+          descriptors->pOutMemspace = H5Screate_simple(1, &dimOut, NULL);
+          if( descriptors->qOutDataset<0 || descriptors->uOutDataset<0 || descriptors->pOutDataset<0 ||
+              descriptors->qOutDataspace<0 || descriptors->uOutDataspace<0 || descriptors->pOutDataspace<0 ||
+              descriptors->qOutMemspace<0 || descriptors->uOutMemspace<0 || descriptors->pOutMemspace<0 ) {
              printf("\nError: HDF5 output allocation failed\n");
              exit(FAILURE);
           }
@@ -231,7 +228,7 @@ int doRMSynthesis(struct optionsList *inOptions,
     nDec = params->qAxisLen2;
     nFrequencies = params->qAxisLen3;
     nPhi = inOptions->nPhi;
-    nInElements = nInFrequencies * nRA;
+    nInElements = nFrequencies * nRa;
     nOutElements= nPhi * nRa;
 
 
@@ -246,10 +243,10 @@ int doRMSynthesis(struct optionsList *inOptions,
     allocateDeviceMemoryForComputation(selectedDeviceInfo.deviceID, &d_lambdaDiff2, &d_phiAxis,
     		&d_qImageArray, &d_uImageArray,
     		&d_qPhi, &d_uPhi, &d_pPhi,
-			nInElements, nOutElements, nRa, nPhi){){
+			nInElements, nOutElements, nRa, nPhi);
 
     /* Compute \lambda^2 - \lambda^2_0 once. Common for all threads */
-	computeLambdaSquareDifference(lambdaDiff2, data_arrays->lambda2, data_arrays->lambda20, nFrequencies);
+	computeLambdaSquareDifference(lambdaDiff2, data_arrays->lambda2, params->lambda20, nFrequencies);
 
 
     t->stopProc = clock();
@@ -284,14 +281,14 @@ int doRMSynthesis(struct optionsList *inOptions,
              break;
           case HDF5:
              offsetIn[1] = j-1;
-             qerror = H5Sselect_hyperslab(descriptors.qDataspace, H5S_SELECT_SET, offsetIn,
+             qerror = H5Sselect_hyperslab(descriptors->qDataspace, H5S_SELECT_SET, offsetIn,
                                     NULL, countIn, NULL);
-             uerror = H5Sselect_hyperslab(descriptors.uDataspace, H5S_SELECT_SET, offsetIn,
+             uerror = H5Sselect_hyperslab(descriptors->uDataspace, H5S_SELECT_SET, offsetIn,
                                     NULL, countIn, NULL);
-             h5ErrorQ = H5Dread(descriptors.qDataset, H5T_NATIVE_FLOAT, qMemspace,
-                                   descriptors.qDataspace, H5P_DEFAULT, qImageArray);
-             h5ErrorU = H5Dread(descriptors.uDataset, H5T_NATIVE_FLOAT, uMemspace,
-                                   descriptors.uDataspace, H5P_DEFAULT, uImageArray);
+             h5ErrorQ = H5Dread(descriptors->qDataset, H5T_NATIVE_FLOAT, descriptors->qMemspace,
+                                   descriptors->qDataspace, H5P_DEFAULT, qImageArray);
+             h5ErrorU = H5Dread(descriptors->uDataset, H5T_NATIVE_FLOAT, descriptors->uMemspace,
+                                   descriptors->uDataspace, H5P_DEFAULT, uImageArray);
              if(h5ErrorQ<0 || h5ErrorU<0 || qerror<0 || uerror<0 ) {
                 printf("\nError: Unable to read input data cubes\n\n");
                 exit(FAILURE);
@@ -304,7 +301,7 @@ int doRMSynthesis(struct optionsList *inOptions,
        /* Transfer input images to device */
        t->startX = clock();
 
-       copyStepToDevice(selectedDeviceInfo.deviceID, qImageArray, d_qImageArray uImageArray, d_uImageArray, nInElements);
+       copyStepToDevice(selectedDeviceInfo.deviceID, qImageArray, d_qImageArray, uImageArray, d_uImageArray, nInElements);
 
        t->stopX = clock();
        t->msX += ((float)(t->stopX - t->startX))/CLOCKS_PER_SEC;
@@ -332,6 +329,8 @@ int doRMSynthesis(struct optionsList *inOptions,
        cudaMemcpy(qPhi, d_qPhi, nOutElements*sizeof(*qPhi), cudaMemcpyDeviceToHost);
        cudaMemcpy(uPhi, d_uPhi, nOutElements*sizeof(*qPhi), cudaMemcpyDeviceToHost);
        cudaMemcpy(pPhi, d_pPhi, nOutElements*sizeof(*qPhi), cudaMemcpyDeviceToHost);
+       copyStepToHost(selectedDeviceInfo.deviceID, qPhi, d_qPhi, uPhi, d_uPhi, pPhi, d_pPhi, nOutElements);
+
        t->stopX = clock();
        t->msX += ((float)(t->stopX - t->startX))/CLOCKS_PER_SEC;
 
@@ -346,18 +345,18 @@ int doRMSynthesis(struct optionsList *inOptions,
              break;
           case HDF5:
              offsetOut[1] = j-1;
-             qerror = H5Sselect_hyperslab(descriptors.qOutDataspace, H5S_SELECT_SET,
+             qerror = H5Sselect_hyperslab(descriptors->qOutDataspace, H5S_SELECT_SET,
                                           offsetOut, NULL, countOut, NULL);
-             uerror = H5Sselect_hyperslab(descriptors.uOutDataspace, H5S_SELECT_SET,
+             uerror = H5Sselect_hyperslab(descriptors->uOutDataspace, H5S_SELECT_SET,
                                           offsetOut, NULL, countOut, NULL);
-             perror = H5Sselect_hyperslab(descriptors.pOutDataspace, H5S_SELECT_SET,
+             perror = H5Sselect_hyperslab(descriptors->pOutDataspace, H5S_SELECT_SET,
                                           offsetOut, NULL, countOut, NULL);
-             h5ErrorQ = H5Dwrite(descriptors.qOutDataset, H5T_NATIVE_FLOAT, descriptors.qOutMemspace,
-                                 descriptors.qOutDataspace, H5P_DEFAULT, qPhi);
-             h5ErrorU = H5Dwrite(descriptors.uOutDataset, H5T_NATIVE_FLOAT, descriptors.uOutMemspace,
-                                 descriptors.uOutDataspace, H5P_DEFAULT, uPhi);
-             h5ErrorP = H5Dwrite(descriptors.pOutDataset, H5T_NATIVE_FLOAT, descriptors.pOutMemspace,
-                                 descriptors.pOutDataspace, H5P_DEFAULT, pPhi);
+             h5ErrorQ = H5Dwrite(descriptors->qOutDataset, H5T_NATIVE_FLOAT, descriptors->qOutMemspace,
+                                 descriptors->qOutDataspace, H5P_DEFAULT, qPhi);
+             h5ErrorU = H5Dwrite(descriptors->uOutDataset, H5T_NATIVE_FLOAT, descriptors->uOutMemspace,
+                                 descriptors->uOutDataspace, H5P_DEFAULT, uPhi);
+             h5ErrorP = H5Dwrite(descriptors->pOutDataset, H5T_NATIVE_FLOAT, descriptors->pOutMemspace,
+                                 descriptors->pOutDataspace, H5P_DEFAULT, pPhi);
              if(h5ErrorQ<0 || h5ErrorU || h5ErrorP<0 ||
                 qerror<0 || uerror<0 || perror<0 ) {
                 printf("\nError: Unable to write output data cubes\n\n");
@@ -381,14 +380,14 @@ int doRMSynthesis(struct optionsList *inOptions,
        free(fPixel);
        break;
     case HDF5:
-       H5Sclose(descriptors.qMemspace);  H5Sclose(descriptors.uMemspace);
-       H5Sclose(descriptors.qDataspace); H5Sclose(descriptors.uDataspace);
-       H5Dclose(descriptors.qDataset);   H5Dclose(descriptors.uDataset);
-       H5Sclose(descriptors.qOutMemspace);  H5Sclose(descriptors.uOutMemspace);
-       H5Sclose(descriptors.qOutDataspace); H5Sclose(descriptors.uOutDataspace);
-       H5Dclose(descriptors.qOutDataset);   H5Dclose(descriptors.uOutDataset);
-       H5Sclose(descriptors.pOutMemspace); H5Sclose(descriptors.pOutDataspace);
-       H5Dclose(descriptors.pOutDataset);
+       H5Sclose(descriptors->qMemspace);  H5Sclose(descriptors->uMemspace);
+       H5Sclose(descriptors->qDataspace); H5Sclose(descriptors->uDataspace);
+       H5Dclose(descriptors->qDataset);   H5Dclose(descriptors->uDataset);
+       H5Sclose(descriptors->qOutMemspace);  H5Sclose(descriptors->uOutMemspace);
+       H5Sclose(descriptors->qOutDataspace); H5Sclose(descriptors->uOutDataspace);
+       H5Dclose(descriptors->qOutDataset);   H5Dclose(descriptors->uOutDataset);
+       H5Sclose(descriptors->pOutMemspace); H5Sclose(descriptors->pOutDataspace);
+       H5Dclose(descriptors->pOutDataset);
        H5Fclose(descriptors->qDirtyH5);
        H5Fclose(descriptors->uDirtyH5);
        H5Fclose(descriptors->pDirtyH5);
